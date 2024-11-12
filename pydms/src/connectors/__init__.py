d45c599377a05877c57f7dc3be254b100eddb9cf
iabaako <iabaako@poverty-action.org>
2024-11-12 10:46:07 +0000

    scto_connector_new

--- pydms/src/connectors/__init__.py
+++ pydms/src/connectors/__init__.py
@@ -1,5 +1,5 @@
 from .scto import scto_import_data, scto_server_connect, scto_load_login, \
-                  scto_load_forms, scto_login_form, scto_download_action
+                  scto_load_forms, scto_login_form, scto_download_action, scto_forms_edit
 from .local import get_excel_sheet_names, local_load_files, local_read_data, \
                    local_add_form, local_load_action
 from .script import script_add_form, script_load_action, script_load_files
