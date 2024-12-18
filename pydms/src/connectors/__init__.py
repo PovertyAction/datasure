cf14e1f07cf7d9c95af6e66d437f7eb7e2739c69
Niall Keleher <nkeleher@poverty-action.org>
2024-12-18 12:42:22 -0800

    format and lint pydms/src/connectors

--- pydms/src/connectors/__init__.py
+++ pydms/src/connectors/__init__.py
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 from .scto import scto_import_data, scto_server_connect, scto_load_login, scto_load_forms
 <<<<<<< HEAD
 from .local import get_excel_sheet_names
@@ -12,3 +13,40 @@ from .local import get_excel_sheet_names, local_load_files, local_read_data, \
                    local_add_form, local_load_action
 from .script import script_add_form, script_load_action, script_load_files
 >>>>>>> 9b1a5b9 (prep)
+=======
+from .local import (
+    get_excel_sheet_names,
+    local_add_form,
+    local_load_action,
+    local_load_files,
+    local_read_data,
+)
+from .script import script_add_form, script_load_action, script_load_files
+from .scto import (
+    scto_download_action,
+    scto_forms_edit,
+    scto_import_data,
+    scto_load_forms,
+    scto_load_login,
+    scto_login_form,
+    scto_server_connect,
+)
+
+__all__ = [
+    "get_excel_sheet_names",
+    "local_add_form",
+    "local_load_action",
+    "local_load_files",
+    "local_read_data",
+    "script_add_form",
+    "script_load_action",
+    "script_load_files",
+    "scto_download_action",
+    "scto_forms_edit",
+    "scto_import_data",
+    "scto_load_forms",
+    "scto_load_login",
+    "scto_login_form",
+    "scto_server_connect",
+]
+>>>>>>> 5efff5e (format and lint pydms/src/connectors)
