a5ebaa48dff41fb069c795a850e79409bf708b99
Niall Keleher <nkeleher@poverty-action.org>
2024-12-18 15:38:41 -0800

    format and lint pydms/src/views

--- pydms/views/output_view_1.py
+++ pydms/views/output_view_1.py
@@ -1,40 +1,33 @@
 import streamlit as st
-import pandas as pd
-from datetime import datetime
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-from src.checks import summary_report, missing_report, progress_report
-
+from src.checks import missing_report, progress_report, summary_report
 
 # define page number
 page_number = 1
 
-st.title(st.session_state[f'config_page_{page_number}'])
+st.title(st.session_state[f"config_page_{page_number}"])
 
-summary, survey_progress, duplicates, missing, outliers, enum_stats = \
-    st.tabs(("Summary", "Survey Progress", "Duplicates", "Missing Data", "Outliers", "Enumerator Stats"))
+summary, survey_progress, duplicates, missing, outliers, enum_stats = st.tabs(
+    (
+        "Summary",
+        "Survey Progress",
+        "Duplicates",
+        "Missing Data",
+        "Outliers",
+        "Enumerator Stats",
+    )
+)
 
 alias_list = list(filter(None, st.session_state.alias_list))
 
-# load data from 
+# load data from
 
 with summary:
-    
-    summary_report(st.session_state[f'prepped_data{page_number}']) 
+    summary_report(st.session_state[f"prepped_data{page_number}"])
 
 
 with missing:
+    missing_report(st.session_state[f"prepped_data{page_number}"])
 
-    missing_report(st.session_state[f'prepped_data{page_number}'])
 
-
-                    
 with survey_progress:
-    
-    progress_report(st.session_state[f'prepped_data{page_number}'])
-
-           
-  
-   
-    
+    progress_report(st.session_state[f"prepped_data{page_number}"])
