f7ca73331cea8efaa53f4a3046a5debe6dcf0c7a
Niall Keleher <nkeleher@poverty-action.org>
2024-12-18 12:43:53 -0800

    format and lint pydms/src/processing

--- pydms/src/processing/__init__.py
+++ pydms/src/processing/__init__.py
@@ -1,1 +1,3 @@
 from .prep import prep_load_log
+
+__all__ = ["prep_load_log"]
--- pydms/src/processing/prep.py
+++ pydms/src/processing/prep.py
@@ -2,21 +2,18 @@ import pandas as pd
 
 
 def prep_load_log(label) -> pd.DataFrame:
-	
-	"""
-	Load existing log or return empty dataframe
+    """Load existing log or return empty dataframe.
 
-	PARAMS:
-	-------
-	return: pandas dataframe of logs
-	"""
-	
-	# load form details from last session
-	try:
-		file = pd.read_json(f'cache/pyDMS_prep_cache_{label}.json')
-		logs = file.to_dict()
-		return pd.DataFrame(logs)
+    PARAMS:
+    -------
+    return: pandas dataframe of logs
+    """
+    # load form details from last session
+    try:
+        file = pd.read_json(f"cache/pyDMS_prep_cache_{label}.json")
+        logs = file.to_dict()
+        return pd.DataFrame(logs)
 
-	# if file not found, return empty dataframe
-	except FileNotFoundError:
-		return pd.DataFrame(columns = ['action', 'description'])
+    # if file not found, return empty dataframe
+    except FileNotFoundError:
+        return pd.DataFrame(columns=["action", "description"])
