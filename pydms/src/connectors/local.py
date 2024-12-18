5efff5e89ac90e75a273e7333dce2cccde5a625b
Niall Keleher <nkeleher@poverty-action.org>
2024-12-18 12:42:22 -0800

    format and lint pydms/src/connectors

--- pydms/src/connectors/local.py
+++ pydms/src/connectors/local.py
@@ -1,218 +1,225 @@
-from io import StringIO
-import streamlit as st
-import pandas as pd
-import time
 import os
 import re
-import zipfile
 import tempfile
+import time
+import zipfile
+
+import pandas as pd
+import streamlit as st
 
 # --- Get List of sheet from excel ---#
 
+
 def get_excel_sheet_names(file_path: str) -> list:
-    
-	"""
-	SOURCES:
-	Code is from the following source:
-	https://stackoverflow.com/questions/20105118/extracting-list-of-sheet-names-from-openpyxl
+    """Import an excel file and return the list of sheet names.
 
-	Import an excel file and return the list of sheet names
+    SOURCES:
+    Code is from the following source:
+    https://stackoverflow.com/questions/20105118/extracting-list-of-sheet-names-from-openpyxl
 
-	PARAMS:
-	-------
-	file_path: str : path to the excel file
+    PARAMS:
+    -------
+    file_path: str : path to the excel file
     """
-
-	sheets = []
-	with zipfile.ZipFile(file_path, 'r') as zip_ref: xml = zip_ref.read("xl/workbook.xml").decode("utf-8")
-	for s_tag in  re.findall("<sheet [^>]*", xml) : sheets.append(  re.search('name="[^"]*', s_tag).group(0)[6:])
-	return sheets
+    sheets = []
+    with zipfile.ZipFile(file_path, "r") as zip_ref:
+        xml = zip_ref.read("xl/workbook.xml").decode("utf-8")
+    for s_tag in re.findall("<sheet [^>]*", xml):
+        sheets.append(re.search('name="[^"]*', s_tag).group(0)[6:])
+    return sheets
 
 
 # --- Create empty Dataframe ---#
 
+
 def local_load_files() -> pd.DataFrame:
-	
-	"""
-	Load files from last session or return empty dataframe
+    """Load files from last session or return empty dataframe.
 
-	PARAMS:
-	-------
-	return: pandas dataframe of file list
-	"""
-	
-	# load form details from last session
-	try:
-		file = pd.read_json('cache/pyDMS_local_files_cache.json')
-		form_inputs = file.to_dict()
-		return pd.DataFrame(form_inputs)
+    PARAMS:
+    -------
+    return: pandas dataframe of file list
+    """
+    # load form details from last session
+    try:
+        file = pd.read_json("cache/pyDMS_local_files_cache.json")
+        form_inputs = file.to_dict()
+        return pd.DataFrame(form_inputs)
 
-	# if file not found, return empty dataframe
-	except FileNotFoundError:
-		return pd.DataFrame(columns = ['alias', 'load', 'type', 'filename', 'sheet name'])
+    # if file not found, return empty dataframe
+    except FileNotFoundError:
+        return pd.DataFrame(columns=["alias", "load", "type", "filename", "sheet name"])
+
 
 # --- Read data from file ---#
 
+
 def local_read_data(filename: str, sheet_name: str = None) -> pd.DataFrame:
+    """Import data from a file.
 
-	"""
-	Import data from a file
+    PARAMS:
+    -------
+    filename: str : path to the file
+    sheet_name: str : name of the sheet to import (only for excel files)
 
-	PARAMS:
-	-------
-	filename: str : path to the file
-	sheet_name: str : name of the sheet to import (only for excel files)
+    Returns
+    -------
+    data: pd.DataFrame : imported data
 
-	RETURNS:
-	--------
-	data: pd.DataFrame : imported data
-	"""
+    """
+    # get file extension
+    fileext = filename.split(".")[-1]
 
+    # import file depending on the file extension
+    if fileext == "csv":
+        data = pd.read_csv(filename)
+    elif fileext in ["xlsx", "xls"]:
+        data = pd.read_excel(filename, sheet_name=sheet_name)
+    elif fileext == "json":
+        data = pd.read_json(filename)
+    elif fileext == "dta":
+        data = pd.read_stata(filename)
 
-	# get file extension
-	fileext = filename.split('.')[-1]
+    return data
 
-	# import file depending on the file extension
-	if fileext == 'csv':
-		data = pd.read_csv(filename)
-	elif fileext in ['xlsx', 'xls']:
-		data = pd.read_excel(filename, sheet_name=sheet_name)
-	elif fileext == 'json':
-		data = pd.read_json(filename)
-	elif fileext == 'dta':
-		data = pd.read_stata(filename)
-
-	return data
 
 # --- get file name and path from st.file_uploader ---#
 
+
 def get_file_path(file_uploader: object) -> str:
+    """Get file path from st.file_uploader.
 
-	"""
-	Get file path from st.file_uploader
+    PARAMS:
+    -------
+    file_uploader: object : st.file_uploader object
 
-	PARAMS:
-	-------
-	file_uploader: object : st.file_uploader object
+    Returns
+    -------
+    file_path: str : path to the file
 
-	RETURNS:
-	--------
+    """
+    temp_dir = tempfile.mkdtemp()
+    path = os.path.join(temp_dir, file_uploader.name)
 
-	file_path: str : path to the file
-	"""
-
-	temp_dir = tempfile.mkdtemp()
-	path = os.path.join(temp_dir, file_uploader.name)
-
-	return path
+    return path
 
 
 # --- FORM for Adding file from local storage ---#
 
+
 def local_add_form() -> None:
+    """Form for adding a file from local storage.
 
-	"""
-	Form for adding a file from local storage
+    PARAMS:
+    -------
+    None
 
-	PARAMS:
-	-------
-	None
+    Returns
+    -------
+    None
 
-	RETURNS:
-	--------
-	None
-	
-	"""
-	
-	st.image("asserts/storage.png", width = 100)
-				
-	st.markdown("Add a new file")
-	# input file alias
-	local_file_alias = st.text_input(label = "alias*", 
-								help = "Enter a unique name for the file")
+    """
+    st.image("asserts/storage.png", width=100)
 
-	# file uploader. Limit to 1 file and allow only file types selected
-	local_added_file = st.text_input(label = "file path*", 
-								help = "Add full file name and path. eg. C:/data/survey.dta")
+    st.markdown("Add a new file")
+    # input file alias
+    local_file_alias = st.text_input(
+        label="alias*", help="Enter a unique name for the file"
+    )
 
-	if local_added_file:
+    # file uploader. Limit to 1 file and allow only file types selected
+    local_added_file = st.text_input(
+        label="file path*", help="Add full file name and path. eg. C:/data/survey.dta"
+    )
 
-		# get file extension from filename
-		local_added_file_ext = local_added_file.split(".")[-1]
+    if local_added_file:
+        # get file extension from filename
+        local_added_file_ext = local_added_file.split(".")[-1]
 
-		# check file validity
-		if not os.path.isfile(local_added_file):
-			st.warning("File not found. Please check the file path")
-		elif local_added_file_ext not in ['csv', 'xlsx', 'xls', 'json', 'dta']:
-			st.warning("Invalid file type. Please upload a valid file type")
-		elif local_added_file_ext in ['xlsx', 'xls']:		
-			sheets  = get_excel_sheet_names(local_added_file)
-			local_added_file_sheet_name = st.selectbox(label = "Sheet Name", 
-										options = sheets)
-		else:
-			local_added_file_sheet_name = None
+        # check file validity
+        if not os.path.isfile(local_added_file):
+            st.warning("File not found. Please check the file path")
+        elif local_added_file_ext not in ["csv", "xlsx", "xls", "json", "dta"]:
+            st.warning("Invalid file type. Please upload a valid file type")
+        elif local_added_file_ext in ["xlsx", "xls"]:
+            sheets = get_excel_sheet_names(local_added_file)
+            local_added_file_sheet_name = st.selectbox(
+                label="Sheet Name", options=sheets
+            )
+        else:
+            local_added_file_sheet_name = None
 
-	# add a submit button
-	local_add_file = st.button("Add File", type = "primary", 
-				 use_container_width = True,
-				 key = "add_file_key",
-				 disabled = not local_added_file and not local_file_alias)
-	
-	st.markdown("**required*")
-	
+    # add a submit button
+    local_add_file = st.button(
+        "Add File",
+        type="primary",
+        use_container_width=True,
+        key="add_file_key",
+        disabled=not local_added_file and not local_file_alias,
+    )
 
-	# if submit (local_add_file) button is clicked
-	
-	if local_add_file:
+    st.markdown("**required*")
 
-		new_file = pd.DataFrame(data = [[local_file_alias, True, local_added_file_ext, local_added_file, local_added_file_sheet_name]], 
-								columns = ['alias', 'load', 'type', 'filename', 'sheet name'])
-		
-		#cst.write(new_file)
-		
-		st.session_state.local_files = pd.concat([st.session_state.local_files, new_file], ignore_index = True)	
-		
+    # if submit (local_add_file) button is clicked
+
+    if local_add_file:
+        new_file = pd.DataFrame(
+            data=[
+                [
+                    local_file_alias,
+                    True,
+                    local_added_file_ext,
+                    local_added_file,
+                    local_added_file_sheet_name,
+                ]
+            ],
+            columns=["alias", "load", "type", "filename", "sheet name"],
+        )
+
+        # cst.write(new_file)
+
+        st.session_state.local_files = pd.concat(
+            [st.session_state.local_files, new_file], ignore_index=True
+        )
+
 
 # --- Load data from local storage ---#
 
+
 def local_load_action(local_inputs: pd.DataFrame) -> None:
+    """Load data from local storage.
 
-	"""
-	Load data from local storage
+    PARAMS:
+    -------
+    local_inputs: pd.DataFrame : file list dataframe
 
-	PARAMS:
-	-------
-	local_inputs: pd.DataFrame : file list dataframe
+    Returns
+    -------
+    None
 
-	RETURNS:
-	--------
-	None
-	
-	"""
+    """
+    # remove empty rows
+    local_inputs = local_inputs[local_inputs["load"] == True]  # noqa: E712
 
-	# remove empty rows
-	local_inputs = local_inputs[local_inputs['load'] == True]
+    # Check data and flag errors
+    if local_inputs.empty:
+        st.warning("No data selected for download. Please select data to download")
+        st.stop()
 
-	# Check data and flag errors
-	if local_inputs.empty:
-		st.warning("No data selected for download. Please select data to download")
-		st.stop()
+    else:
+        local_files_count = len(local_inputs.index)
 
-	else:
+        # download data
+        for i in range(0, local_files_count):
+            if f"local_raw_data{i}" in st.session_state:
+                local_filename = local_inputs["filename"][i]
+                local_sheet_name = local_inputs["sheet name"][i]
 
-		local_files_count = len(local_inputs.index)
+                st.session_state[f"local_raw_data{i}"] = local_read_data(
+                    local_filename, local_sheet_name
+                )
+                time.sleep(1)
 
-		# download data
-		for i in range(0, local_files_count):
-			if f'local_raw_data{i}' in st.session_state:
-				
-				local_filename = local_inputs['filename'][i]
-				local_sheet_name = local_inputs['sheet name'][i]
-				
-				st.session_state[f'local_raw_data{i}'] = local_read_data(local_filename, local_sheet_name)
-				time.sleep(1)
+        # modify session state for preview
+        st.session_state.local_show_preview = True
 
-		# modify session state for preview
-		st.session_state.local_show_preview = True	
-
-		st.success(f'{local_files_count} Datasets loaded from local storage')
+        st.success(f"{local_files_count} Datasets loaded from local storage")
--- pydms/src/connectors/script.py
+++ pydms/src/connectors/script.py
@@ -1,128 +1,148 @@
-import streamlit as st
-import pandas as pd
 import os
 import time
-import importlib.util
+
+import pandas as pd
+import streamlit as st
 
 # --- Import details from prev session or create empty dataframe ---#
 
+
 def script_load_files() -> pd.DataFrame:
-	
-	"""
-	Load files from last session or return empty dataframe
+    """Load files from last session or return empty dataframe.
 
-	PARAMS:
-	-------
-	return: pandas dataframe of file list
-	"""
-	
-	# load form details from last session
-	try:
-		file = pd.read_json('cache/pyDMS_script_files_cache.json')
-		form_inputs = file.to_dict()
-		return pd.DataFrame(form_inputs)
+    PARAMS:
+    -------
+    return: pandas dataframe of file list
+    """
+    # load form details from last session
+    try:
+        file = pd.read_json("cache/pyDMS_script_files_cache.json")
+        form_inputs = file.to_dict()
+        return pd.DataFrame(form_inputs)
 
-	# if file not found, return empty dataframe
-	except FileNotFoundError:
-		return pd.DataFrame(columns = ['alias', 'load', 'type', 'filename', 'dataset'])
+    # if file not found, return empty dataframe
+    except FileNotFoundError:
+        return pd.DataFrame(columns=["alias", "load", "type", "filename", "dataset"])
 
 
 # --- FORM for adding new script --- #
 
+
 def script_add_form() -> None:
+    """Form for adding a new script.
 
-	"""
-	Form for adding a new script
+    PARAMS:
+    -------
+    None
 
-	PARAMS:
-	-------
-	None
+    Returns
+    -------
+    None
 
-	RETURNS:
-	--------
-	None
-	"""
+    """
+    st.image("asserts/python.png", width=100)
+    st.markdown("Add a python script")
 
-	st.image("asserts/python.png", width = 100)
-	st.markdown("Add a python script")
-	
-	# input file alias
-	script_alias = st.text_input(label = "alias*", 
-								help = "Enter a unique name for the file", 
-								key = "script_alias_key")
+    # input file alias
+    script_alias = st.text_input(
+        label="alias*", help="Enter a unique name for the file", key="script_alias_key"
+    )
 
-	# file uploader. Limit to 1 file and allow only file types selected
-	added_script = st.text_input(label = "file path*", 
-							  			help = "Add full file name and path. eg. C:/data/kobo_import.py", 
-										key = "added_script_key")
-	
-	if added_script:
+    # file uploader. Limit to 1 file and allow only file types selected
+    added_script = st.text_input(
+        label="file path*",
+        help="Add full file name and path. eg. C:/data/kobo_import.py",
+        key="added_script_key",
+    )
 
-		# get file extension from filename
-		added_script_ext = added_script.split(".")[-1]
+    if added_script:
+        # get file extension from filename
+        added_script_ext = added_script.split(".")[-1]
 
-		if not os.path.isfile(added_script):
-			st.warning("File not found. Please enter a valid file path")
-		elif added_script_ext != 'py':
-			st.warning("Invalid file type. Please upload a valid file type")
-	
-	# get name of dataset to input from script
-	added_script_data = st.text_input(label = "Dataset*", 
-							  			help = "Specify the dataset to be loaded from script", 
-										key = "added_script_data_key")
-	
-	st.markdown("**required*")
-	
-	# add a submit button
-	add_script_btn = st.button("Add Script", type = "primary", 
-											 use_container_width = True,
-											 key = "add_script_btn_key",
-											 disabled = not added_script or not script_alias or not added_script_data)
-	# add_file action
-	if add_script_btn:
+        if not os.path.isfile(added_script):
+            st.warning("File not found. Please enter a valid file path")
+        elif added_script_ext != "py":
+            st.warning("Invalid file type. Please upload a valid file type")
 
-		new_script = pd.DataFrame(data = [[script_alias, True, added_script_ext, added_script, added_script_data]], 
-										columns = ['alias', 'load', 'type', 'filename', 'dataset'])
-				
-		st.session_state.script_files = pd.concat([st.session_state.script_files, new_script], ignore_index = True)
+    # get name of dataset to input from script
+    added_script_data = st.text_input(
+        label="Dataset*",
+        help="Specify the dataset to be loaded from script",
+        key="added_script_data_key",
+    )
+
+    st.markdown("**required*")
+
+    # add a submit button
+    add_script_btn = st.button(
+        "Add Script",
+        type="primary",
+        use_container_width=True,
+        key="add_script_btn_key",
+        disabled=not added_script or not script_alias or not added_script_data,
+    )
+    # add_file action
+    if add_script_btn:
+        new_script = pd.DataFrame(
+            data=[
+                [script_alias, True, added_script_ext, added_script, added_script_data]
+            ],
+            columns=["alias", "load", "type", "filename", "dataset"],
+        )
+
+        st.session_state.script_files = pd.concat(
+            [st.session_state.script_files, new_script], ignore_index=True
+        )
+
 
 # --- Add Script Action --- #
 
+
 def script_load_action(script_inputs: dict) -> None:
-	
-	# remove empty rows
-	script_inputs = script_inputs[script_inputs['load'] == True]
+    """Load data from scripts based on the provided inputs.
 
-	# Check data and flag errors
-	if script_inputs.empty:
-		st.warning("No data selected for download. Please select data to download")
-		st.stop()
+    PARAMS:
+    -------
+    script_inputs: dict
+        Dictionary containing script input details.
 
-	form_count = len(script_inputs.index)
+    Returns
+    -------
+    None
 
-	st.progress(0, text = "Loading data From Script ...")
+    """
+    # remove empty rows
+    script_inputs = script_inputs[script_inputs["load"] == True]  # noqa: E712
 
-	st.write(f'Loading {form_count} datasets from Script')
+    # Check data and flag errors
+    if script_inputs.empty:
+        st.warning("No data selected for download. Please select data to download")
+        st.stop()
 
-	# download data
-	for i in range(0, form_count):
+    form_count = len(script_inputs.index)
 
-		if f'script_raw_data{i}' in st.session_state:
-			
-			# get filename & path
-			filename = script_inputs['filename'][i]
-			dataset = script_inputs['dataset'][i]
+    st.progress(0, text="Loading data From Script ...")
 
-			st.write('Name', exec(open(filename).read()))
-			
-        	# Check if the module has a DataFrame named {dataset}
-			#if hasattr(exec_file, dataset):
-				#st.session_state[f'script_raw_data{i}'] = getattr(exec_file, dataset)
+    st.write(f"Loading {form_count} datasets from Script")
 
-			time.sleep(3)
-			st.write(f'{i + 1}/{form_count}: Loaded successfully')
+    # download data
+    for i in range(0, form_count):
+        if f"script_raw_data{i}" in st.session_state:
+            # get filename & path
+            filename = script_inputs["filename"][i]
+            # dataset = script_inputs["dataset"][i]
 
-	st.success("Data load from scripts complete")
+            with open(filename) as file:
+                st.write("Name", exec(file.read()))
 
-	# modify session state for preview
-	st.session_state.scto_show_preview = True
+            # Check if the module has a DataFrame named {dataset}
+            # if hasattr(exec_file, dataset):
+            # st.session_state[f'script_raw_data{i}'] = getattr(exec_file, dataset)
+
+            time.sleep(3)
+            st.write(f"{i + 1}/{form_count}: Loaded successfully")
+
+    st.success("Data load from scripts complete")
+
+    # modify session state for preview
+    st.session_state.scto_show_preview = True
