4b63affb1e5f92c1b7998dfe9cad7c6c9576e723
iabaako <iabaako@poverty-action.org>
2024-12-20 11:15:45 +0000

    added progress dashboard from mx team

--- src/checks/progress.py
+++ src/checks/progress.py
@@ -22,31 +22,31 @@ def progress_report(data) -> None:
 		meta_col, enum_col, agg_col = st.columns(spec = 3, border= True)
 
 		with meta_col:
-			duration = st.selectbox("Duration", options = survey_cols, help = "Column containing survey duration", key = "duration_progress")
-			date = st.selectbox("Date", options = survey_cols, help = "Column containing survey date", key = "date_progress")
-			formversion = st.selectbox("Form Version", options = survey_cols, help = "Column containing survey form version", key = "formversion_progress")
+			duration = st.selectbox("Duration", options = survey_cols, help = "Column containing survey duration", key = "duration_progress", index=None)
+			date = st.selectbox("Date", options = survey_cols, help = "Column containing survey date", key = "date_progress", index=None)
+			formversion = st.selectbox("Form Version", options = survey_cols, help = "Column containing survey form version", key = "formversion_progress", index=None)
 
 		with enum_col:
-			by = st.selectbox("Group by", options = survey_cols, help = "Column to group summary report by by", key = "groupby_progress")
-			enumerator = st.selectbox("Enumerator", options = survey_cols, help = "Column containing survey enumerator", key = "enumerator_progress")
-			team = st.selectbox("Team", options = survey_cols, help = "Column containing survey team", key = "team_progress")
+			by = st.selectbox("Group by", options = survey_cols, help = "Column to group summary report by by", key = "groupby_progress", index=None)
+			enumerator = st.selectbox("Enumerator", options = survey_cols, help = "Column containing survey enumerator", key = "enumerator_progress", index=None)
+			team = st.selectbox("Team", options = survey_cols, help = "Column containing survey team", key = "team_progress", index=None)
 		
 		with agg_col:
 			
-			survey_id = st.selectbox("Survey ID", options = survey_cols, help = "Column containing survey ID", key = "surveyid_progress")
-			survey_key = st.selectbox("Survey Key", options = survey_cols, help = "Column containing survey key", key = "surveykey_progress")
+			survey_id = st.selectbox("Survey ID", options = survey_cols, help = "Column containing survey ID", key = "surveyid_progress", index=None)
+			survey_key = st.selectbox("Survey Key", options = survey_cols, help = "Column containing survey key", key = "surveykey_progress", index=None)
 
-			consent = st.selectbox("Consent", options = survey_cols, help = "Column containing survey consent", key = "consent_progress")
+			consent = st.selectbox("Consent", options = survey_cols, help = "Column containing survey consent", key = "consent_progress", index=None)
 
 			if consent:
 				consent_options = data[consent].unique().tolist()
-				consent_val = st.multiselect("Consent value(s)", options = consent_options, help = "Value(s) indicating valid consent")
+				consent_val = st.multiselect("Consent value(s)", options = consent_options, help = "Value(s) indicating valid consent", key="consent_val_progress")
 
-			outcome = st.selectbox("Outcome", options = survey_cols, help = "Column containing survey outcome")
+			outcome = st.selectbox("Outcome", options = survey_cols, help = "Column containing survey outcome", key="outcome_progress", index=None)
 
 			if outcome:
 				outcome_options = data[outcome].unique().tolist()
-				outcome_val = st.multiselect("Outcome value(s)", options = outcome_options, help = "Value(s) indicating completed survey")
+				outcome_val = st.multiselect("Outcome value(s)", options = outcome_options, help = "Value(s) indicating completed survey", key="outcome_val_progress")
 		
 		st.write("---")
 		st.markdown("### Tracking Options")
@@ -75,13 +75,7 @@ def progress_report(data) -> None:
 			unsafe_allow_html=True,
 		)
 
-		consent_col = st.selectbox(
-			"Select your consent variable",
-			options=data.columns,
-			index=data.columns.get_loc(consent),
-		)
-
-		summary = data.groupby(consent_col)[survey_id].nunique().reset_index()
+		summary = data.groupby(consent)[survey_id].nunique().reset_index()
 		summary.columns = ["Consent Status", "Unique ID Count"]
 
 		st.table(summary)
@@ -91,8 +85,8 @@ def progress_report(data) -> None:
 		data[consent] = data[consent].map(mapping)
 
 		# Group by 'id' and count unique values of "key"
-		unique_counts = df.groupby(survey_id)[survey_key].nunique().reset_index()
+		unique_counts = data.groupby(survey_id)[survey_key].nunique().reset_index()
 		unique_counts.columns = [survey_id, "unique_key_count"]
 
 		# Count unique ids from the new df
-		count_unique_ids = unique_counts["id"].nunique()
+		count_unique_ids = unique_counts[survey_id].nunique()
