746f8042a2b02fcd0fcd007d71ed37a27c3ff3e8
iabaako <iabaako@poverty-action.org>
2025-03-13 08:56:19 +0000

    init commit of changes

--- src/checks/missing.py
+++ src/checks/missing.py
@@ -1,5 +1,8 @@
+import numpy as np
 import pandas as pd
+import plotly.express as px
 import streamlit as st
+from streamlit_extras.stylable_container import stylable_container
 
 
 # define function to create summary report
@@ -59,128 +62,197 @@ def missing_report(data, page_num) -> None:  # noqa: D417, RUF100
 
     st.markdown("## Missing data")
 
-    # Initialize empty lists to store results
-    results = []
+    with stylable_container(
+        key="missing_metrics",
+        css_styles="""
+            {
+                background-color: #F9F9F9;
+                border: 1px solid rgba(49, 51, 63, 0.2);
+                border-radius: 0.5rem;
+                padding: calc(1em - 1px)
+            }
+            """,
+    ):
+        mc1, mc2, mc3, mc4 = st.columns(4)
 
-    for column in data.columns:
-        # Count not coded missing (NA, empty spaces, empty strings)
-        not_coded = (
-            data[column].isna().sum()
-            + (data[column] == " ").sum()
-            + (data[column] == "").sum()
+        missing_values = data[miss_cols].isnull().mean() * 100
+        all_missing = data[miss_cols].isnull().all() * 100
+        any_missing = data[miss_cols].isnull().any() * 100
+        no_missing = 100 - any_missing
+
+        mc1.metric(
+            label="% of missing values",
+            value=f"{missing_values.mean():.2f}%",
         )
 
-        # Initialize dictionary for the row
-        row_dict = {
-            "Variable": column,
-            "Total Missing": not_coded,  # Initialize with not_coded
-            "Not Coded": not_coded,
-        }
+        mc2.metric(
+            label="% of columns with all missing values",
+            value=f"{all_missing.mean():.2f}%",
+        )
 
-        # Add counts and update total for each code/label pair
-        for code, label in zip(missing_codes, missing_labels, strict=False):
-            count = (data[column] == code).sum()
-            row_dict[f"{label} ({code})"] = count
-            row_dict["Total Missing"] += count  # Add to total
+        mc3.metric(
+            label="% of columns with at least one missing value",
+            value=f"{any_missing.mean():.2f}%",
+        )
 
-        # Calculate and add percentages
-        total_rows = len(data)
-        row_dict["Total Missing (%)"] = (row_dict["Total Missing"] / total_rows) * 100
-        row_dict["Not Coded (%)"] = (not_coded / total_rows) * 100
+        mc4.metric(
+            label="% of columns with no missing values",
+            value=f"{no_missing.mean():.2f}%",
+        )
 
-        # Add percentages for each code/label pair
-        for code, label in zip(missing_codes, missing_labels, strict=False):
-            count = row_dict[f"{label} ({code})"]
-            row_dict[f"{label} ({code}) (%)"] = (count / total_rows) * 100
+    # calculate the number of missing values in each column
+    mv_data = data[miss_cols].isnull().sum()
+    mv_data = pd.DataFrame({"Column": mv_data.index, "Null Values": mv_data.values})
+    mv_data["% Null Values"] = (mv_data["Null Values"] / len(data)) * 100
 
-        results.append(row_dict)
-
-    # Create DataFrame from results
-    missing_data = pd.DataFrame(results)
-
-    # Create the slider
-    missing_threshold = st.slider(
-        "Variables with % of missing values above:", 0, 100, 1
-    )
-
-    # Filter based on total missing percentage
-    missing_data_filtered = missing_data[
-        missing_data["Total Missing (%)"] >= missing_threshold
+    mv_data["Total Missing"] = 0
+    mv_data["% Total Missing"] = 0
+    mv_data = mv_data[
+        ["Column", "Total Missing", "% Total Missing", "Null Values", "% Null Values"]
     ]
 
-    if not missing_data_filtered.empty:
-        # Create two tabs
-        tab1, tab2 = st.tabs(["Percentages", "Total Count"])
+    for i, mcode in enumerate(missing_codes):
+        new_col = data[miss_cols].apply(lambda x: x == mcode).sum()  # noqa: B023
+        new_col = pd.DataFrame(
+            {"Column": new_col.index, f"{missing_labels[i]}": new_col.values}
+        )
+        new_col[f"% {missing_labels[i]}"] = (
+            new_col[f"{missing_labels[i]}"] / len(data)
+        ) * 100
 
-        with tab1:
-            # Create list of columns for percentages
-            pct_columns = [
-                "Variable",
-                "Total Missing (%)",
-                "Not Coded (%)",
-            ]
-            pct_columns.extend(
-                [
-                    f"{label} ({code}) (%)"
-                    for code, label in zip(missing_codes, missing_labels, strict=False)
-                ]
-            )
+        # join new column to mv_data using column name
+        mv_data = mv_data.merge(new_col, on="Column", how="left")
+        mv_data["Total Missing"] += mv_data[f"{missing_labels[i]}"]
 
-            # Display percentages DataFrame
-            percentages_df = missing_data_filtered[pct_columns]
+    mv_data["% Total Missing"] = (mv_data["Total Missing"] / len(data)) * 100
 
-            # Create column config for percentages
-            pct_config = {"Variable": st.column_config.Column(width="medium")}
+    # format percentage columns
+    for col in mv_data.columns:
+        if "%" in col:
+            mv_data.style.format({col: "{:.2f}%".format})
 
-            # Add percentage format for all percentage columns
-            for col in pct_columns[1:]:  # Skip 'Column'
-                pct_config[col] = st.column_config.NumberColumn(
-                    format="%.1f%%", width="small"
-                )
+    # display the table
+    st.write("---")
+    st.markdown("## Missingness by column")
 
-            st.dataframe(
-                percentages_df,
-                hide_index=True,
-                use_container_width=True,
-                column_config=pct_config,
-            )
+    _, _, _, slider_col = st.columns(4)
 
-        with tab2:
-            # Create list of columns for counts
-            count_columns = ["Variable", "Total Missing", "Not Coded"]
-            count_columns.extend(
-                [
-                    f"{label} ({code})"
-                    for code, label in zip(missing_codes, missing_labels, strict=False)
-                ]
-            )
+    # Create the slider
+    with slider_col:
+        mv_threshold = st.slider("Variables with % of missing values above:", 0, 100, 1)
 
-            # Display counts DataFrame
-            counts_df = missing_data_filtered[count_columns]
+    # Filter based on total missing percentage
+    mv_data_filtered = mv_data[mv_data["% Null Values"] >= mv_threshold]
 
-            # Create column config for counts
-            count_config = {"Variable": st.column_config.Column(width="medium")}
-
-            # Add number format for all count columns
-            for col in count_columns[1:]:  # Skip 'Column'
-                count_config[col] = st.column_config.NumberColumn(
-                    format="%d", width="small"
-                )
-
-            st.dataframe(
-                counts_df,
-                hide_index=True,
-                use_container_width=True,
-                column_config=count_config,
-            )
-
-    else:
-        st.write(
-            "There are no variables with more than",
-            missing_threshold,
-            "% missing values.",
+    with stylable_container(
+        key="missing_table",
+        css_styles="""
+            {
+                background-color: #F9F9F9;
+                border: 1px solid rgba(49, 51, 63, 0.2);
+                border-radius: 0.5rem;
+            }
+            """,
+    ):
+        st.dataframe(
+            mv_data_filtered.reset_index(drop=True),
+            use_container_width=True,
         )
 
-    st.write(
-        "Note: The calculations for 'Not Coded' includes values of ' . ', 'NA' and empty cells."
+    # missingness over time
+    st.write("---")
+    st.markdown("## Missingness over time")
+
+    # using the submission date column to show missingness over time
+    # get the date columns from dataset
+    date_cols = data.select_dtypes(include=["datetime64"]).columns
+    select_date_col = st.selectbox("Select date column", options=date_cols)
+
+    # extract dateonly from selected date column
+    miss_trend_data = data.copy()
+    miss_trend_data["missingness_trend_date"] = data[select_date_col].dt.date
+    miss_trend_date_count = miss_trend_data["missingness_trend_date"]
+
+    # generate a new dataset aggregating missingness trend date by date count
+    miss_trend_date_count = miss_trend_date_count.value_counts().reset_index()
+
+    # calculate missingness over time
+    missingness_over_time = miss_trend_data.groupby("missingness_trend_date")[
+        miss_cols
+    ].apply(lambda x: x.isnull().sum())
+    missingness_over_time = missingness_over_time.reset_index()
+
+    # get a list of all variables except for missingness_trend_date
+    cols = list(missingness_over_time.columns)
+    cols.remove("missingness_trend_date")
+
+    missingness_over_time["total_missing"] = (
+        missingness_over_time[cols].sum(axis=1).reset_index(drop=True)
     )
+    missingness_over_time = missingness_over_time[
+        ["missingness_trend_date", "total_missing"]
+    ]
+
+    # merge missingness_over_time with miss_trend_date_count
+    missingness_over_time = missingness_over_time.merge(
+        miss_trend_date_count, on="missingness_trend_date", how="left"
+    )
+    missingness_over_time["count"] = missingness_over_time["count"] * len(cols)
+    missingness_over_time["missingness_rate"] = (
+        missingness_over_time["total_missing"] / missingness_over_time["count"]
+    ) * 100
+
+    # display area plot of missingness over time
+    st.area_chart(
+        missingness_over_time.set_index("missingness_trend_date"),
+        y="missingness_rate",
+        use_container_width=True,
+        color=["#FF8000"],
+    )
+
+    ## nullity correlation
+    st.write("---")
+    st.markdown("## Nullity correlation")
+
+    # get default columns to include in the nullity correlation heatmap.
+    # These are columns with at least one missing value and les 100% missing values
+    null_cols = mv_data[
+        (mv_data["% Null Values"] > 0) & (mv_data["% Null Values"] < 100)
+    ]["Column"].tolist()
+
+    # allow users to select columns to include in the nullity correlation heatmap
+    null_cols_sel = st.multiselect(
+        "Select columns to include in the nullity correlation heatmap",
+        options=null_cols,
+    )
+    if null_cols_sel and len(null_cols_sel) > 1:
+        nullity_cols = null_cols_sel
+    else:
+        nullity_cols = null_cols
+
+    # build correlation matrix
+    nullity_corr = data[nullity_cols].isnull().corr()
+    # remove top half of the correlation matrix
+    nullity_corr = nullity_corr.where(
+        np.tril(np.ones(nullity_corr.shape)).astype(np.bool)
+    )
+
+    # define colors for heatmap
+    sns_colormap = [
+        [0.0, "#3f7f93"],
+        [0.1, "#6397a7"],
+        [0.2, "#88b1bd"],
+        [0.3, "#acc9d2"],
+        [0.4, "#d1e2e7"],
+        [0.5, "#f2f2f2"],
+        [0.6, "#f6cdd0"],
+        [0.7, "#efa8ad"],
+        [0.8, "#e8848b"],
+        [0.9, "#e15e68"],
+        [1.0, "#da3b46"],
+    ]
+
+    # create a heatmap
+    fig = px.imshow(nullity_corr, color_continuous_scale=sns_colormap)
+    fig.update_layout(width=1000, height=1000)
+    st.plotly_chart(fig)
