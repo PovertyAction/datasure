5bfb7ae0e20732d1b45bd362798436b9b38e5ad9
iabaako <iabaako@poverty-action.org>
2025-03-20 08:30:32 +0000

    summary setup and submissions functions

--- src/checks/summary.py
+++ src/checks/summary.py
@@ -1,11 +1,11 @@
-from datetime import datetime
-
 import pandas as pd
+import plotly.express as px
 import streamlit as st
+from millify import millify
 
 
 # define function to create summary report
-def summary_report(data, page_num) -> None:
+def summary_settings(data: pd.DataFrame, setting_file: str, page_num) -> tuple:
     """
     Generates a summary report for the survey data
 
@@ -16,7 +16,8 @@ def summary_report(data, page_num) -> None:
 
     Returns
     -------
-    None
+    tuple
+            A tuple containing the settings for the summary report
 
     """
     with st.expander("settings", icon=":material/settings:"):
@@ -85,7 +86,7 @@ def summary_report(data, page_num) -> None:
                 page_num - 1
             ]
             default_survey_id_index = survey_cols.get_loc(default_survey_id)
-            survey_id = st.selectbox(
+            survey_id = st.selectbox(  # noqa: F841
                 label="Survey ID",
                 options=survey_cols,
                 help="Column containing survey ID",
@@ -103,7 +104,7 @@ def summary_report(data, page_num) -> None:
 
             if consent:
                 consent_options = data[consent].unique().tolist()
-                consent_val = st.multiselect(
+                consent_val = st.multiselect(  # noqa: F841
                     label="Consent value(s)",
                     options=consent_options,
                     help="Value(s) indicating valid consent",
@@ -128,25 +129,9 @@ def summary_report(data, page_num) -> None:
         st.write("---")
         st.markdown("### Additional Options")
 
-        if date:
-            st.markdown("##### Filter data by date")
-            # get first and last date from date column in dataset
-            min_date = data[date].min().date()
-            max_date = data[date].max().date()
-            date_filter = st.slider(  # noqa: F841
-                label="Select date range",
-                min_value=min_date,
-                max_value=max_date,
-                format="YYYY-MM-DD",
-                value=(min_date, max_date),
-                label_visibility="collapsed",
-                help="Select date range to filter data",
-                key="date_filter_summary",
-            )
-
         # number of interviews expected
         st.markdown("##### Target number of interviews")
-        total_goal = st.number_input(
+        total_goal = st.number_input(  # noqa: F841
             label="Total goal",
             min_value=0,
             help="Total number of interviews expected",
@@ -157,241 +142,123 @@ def summary_report(data, page_num) -> None:
         # define a save settings button
         save_settings = st.button("Save settings")  # noqa: F841
 
-    # Check that required options have been selected. If not, display a info message
-    if not all([date, enumerator, survey_id, consent, outcome]):
-        st.info("Please select all required columns to generate the summary report")
-        return
+    return date, enumerator
 
-    # Define flagged percentage of missing. For example, write 50 if there are
-    # more than 50% of missing and should be flagged as warning
-    percentage_warning = 50
 
-    ### Value box 1 ###
+def summary_submissions(data: pd.DataFrame, date: str = None) -> None:  # noqa: RUF013
+    """
+    Generates a summary report for the survey data
 
-    # count the number of valid consent
-    valid_interviews = data[consent].isin(consent_val).sum()
+    Parameters
+    ----------
+    data : pd.DataFrame
+            The survey data
 
-    # Calculate porcentage of finished interviews
-    percentage_finished = (valid_interviews / total_goal) * 100
+    date : str
+            The date column in the survey data
 
-    # Format of percentage
-    formatted_percentage_finished = f"{percentage_finished:.2f}%"
+    Returns
+    -------
+    None
+    """
+    st.markdown("## Submission details")
+    if date:
+        mc1, mc2, mc3, mc4 = st.columns(spec=4, border=True)
+        submissions_today = data[data[date] == pd.Timestamp.now().normalize()].shape[0]
+        submissions_yesterday = data[
+            data[date] == pd.Timestamp.now().normalize() - pd.DateOffset(days=1)
+        ].shape[0]
+        submissions_this_week = data[
+            data[date] >= pd.Timestamp.now().normalize() - pd.DateOffset(weeks=1)
+        ].shape[0]
+        submissions_last_week = data[
+            (data[date] >= pd.Timestamp.now().normalize() - pd.DateOffset(weeks=2))
+            & (data[date] < pd.Timestamp.now().normalize() - pd.DateOffset(weeks=1))
+        ].shape[0]
+        submissions_this_month = data[
+            data[date] >= pd.Timestamp.now().normalize() - pd.DateOffset(months=1)
+        ].shape[0]
+        submissions_last_month = data[
+            (data[date] >= pd.Timestamp.now().normalize() - pd.DateOffset(months=2))
+            & (data[date] < pd.Timestamp.now().normalize() - pd.DateOffset(months=1))
+        ].shape[0]
+        submissions_total = data.shape[0]
 
-    ### Value box 2 ###
-    # Identify the date of the first interview
-    earliest_date = data[date].min()
+        submissions_today_delta = (
+            ((submissions_today - submissions_yesterday) / submissions_yesterday) * 100
+            if submissions_yesterday > 0
+            else 0
+        )
+        submissions_this_week_delta = (
+            ((submissions_this_week - submissions_last_week) / submissions_last_week)
+            * 100
+            if submissions_last_week > 0
+            else 0
+        )
+        submissions_this_month_delta = (
+            ((submissions_this_month - submissions_last_month) / submissions_last_month)
+            * 100
+            if submissions_last_month > 0
+            else 0
+        )
 
-    # Todays date
-    today = pd.Timestamp.now()
+        mc1.metric(
+            label="Today",
+            value=submissions_today,
+            delta=millify(submissions_today_delta, precision=2),
+            help="Number of submissions today. Delta is the percentage change from yesterday.",
+        )
+        mc2.metric(
+            label="This week",
+            value=submissions_this_week,
+            delta=millify(submissions_this_week_delta, precision=2),
+            help="Number of submissions this week. Delta is the percentage change from last week.",
+        )
+        mc3.metric(
+            label="This month",
+            value=submissions_this_month,
+            delta=millify(submissions_this_month_delta, precision=2),
+            help="Number of submissions this month. Delta is the percentage change from last month",
+        )
+        mc4.metric(
+            label="Total", value=submissions_total, help="Total number of submissions"
+        )
 
-    # Calculate the number of days since the first interview/launch
-    days_since_start = (today - earliest_date).days
+        submissions_by_date = data.groupby(date).size().reset_index(name="submissions")
+        fig = px.area(
+            submissions_by_date,
+            x=date,
+            y="submissions",
+            title="Submissions by date",
+            color_discrete_sequence=["#e8848b"],
+        )
+        fig.update_layout(width=1000, height=500)
+        fig.update_yaxes(tick0=0, dtick=1)
+        st.plotly_chart(fig, use_container_width=True)
+    else:
+        st.warning("Please select a date column to view submissions details")
 
-    # Set the color
-    color = "black"  # noqa: F841
 
-    ### Value box 3 ###
-    # Percentage of missing ID's
-    total_values = data[survey_id].size
+def summary_report(data: pd.DataFrame, page_num: int) -> None:
+    """
+    Generates a summary report for the survey data
 
-    # Define ID variable
-    missing_values = data[survey_id].isnull().sum()
-    missing_percentage = (missing_values / total_values) * 100
+    Parameters
+    ----------
+    data : pd.DataFrame
+            The survey data
 
-    # Format the percentage
-    formatted_missing_percentage = f"{missing_percentage:.2f}%"
+    settings : dict
+            The settings for the summary report
 
-    ### Value box 4 ###
-    # Group by date and count number of IDs
-    count_by_date = data.groupby(date).size()
+    Returns
+    -------
+    None
+    """
+    page_name = st.session_state.config_pages["Page Name"][page_num - 1]
+    setting_file = f"cache/settings/pyDMS_hfc_settings_{page_name}.json"
 
-    # Calculate the average number of interviews per day
-    average_interviews_per_day = count_by_date.mean()
-
-    # Round number of interviews per day
-    rounded_average_day = round(average_interviews_per_day, 2)
-
-    #### Create first row of value boxes ####
-    # Define color codes
-    color_completed = (
-        "#FFA500" if percentage_finished <= percentage_warning else "#4CAF50"
+    date, _ = summary_settings(data=data, setting_file=setting_file, page_num=page_num)
+    summary_submissions(
+        data=data, date=st.session_state["config_pages"]["Survey Date"][page_num - 1]
     )
-    color_missing = "#FFA500" if missing_percentage > percentage_warning else "#4CAF50"
-
-    # Include Font Awesome
-    st.markdown(
-        '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">',
-        unsafe_allow_html=True,
-    )
-
-    # Create columns for value boxes
-    col1, col2, col3, col4 = st.columns(spec=4, border=True)
-
-    # Value box: Completed interviews
-    with col1:
-        st.markdown(
-            f"""
-                <div style="display: flex; align-items: center; padding: 20px; border: 1px solid #e0e0e0; border-radius: 8px; background-color: #f9f9f9; text-align: center; width: 100%;">
-                    <i class="fas fa-bullseye" style="font-size: 70px; color: {color_completed}; margin-right: 10px;"></i>
-                    <div style="flex-grow: 1;">
-                        <h3 style="margin: 0; font-size: 25px; text-align: center;">Completed Interviews</h3>
-                        <p style="font-size: 53px; color: {color_completed}; margin: 0; text-align: center;">{formatted_percentage_finished}</p>
-                    </div>
-                </div>
-                """,
-            unsafe_allow_html=True,
-        )
-
-        # Value box: Number of days since launch
-    with col2:
-        st.markdown(
-            f"""
-		<div style="display: flex; align-items: center; padding: 20px; border: 1px solid #e0e0e0; border-radius: 8px; background-color: #f9f9f9; text-align: center; width: 100%;">
-			<i class="fas fa-calendar-week" style="font-size: 70px; color: black; margin-right: 10px;"></i>
-			<div style="flex-grow: 1;">
-				<h3 style="margin: 0; font-size: 25px; text-align: center;">Number of Days since Launch</h3>
-				<p style="font-size: 53px; color: black; margin: 0; text-align: center;">{days_since_start}</p>
-			</div>
-		</div>
-		""",
-            unsafe_allow_html=True,
-        )
-
-    # Value box: % of missing IDs
-    with col3:
-        st.markdown(
-            f"""
-		<div style="display: flex; align-items: center; padding: 20px; border: 1px solid #e0e0e0; border-radius: 8px; background-color: #f9f9f9; text-align: center; width: 100%;">
-			<i class="fas fa-percent" style="font-size: 70px; color: {color_missing}; margin-right: 10px;"></i>
-			<div style="flex-grow: 1;">
-				<h3 style="margin: 0; font-size: 25px; text-align: center;">Percentage of Missing IDs</h3>
-				<p style="font-size: 53px; color: {color_missing}; margin: 0; text-align: center;">{formatted_missing_percentage}</p>
-			</div>
-		</div>
-		""",
-            unsafe_allow_html=True,
-        )
-
-    # Value box 4: Average Interviews per Day
-    with col4:
-        st.markdown(
-            f"""
-		<div style="display: flex; align-items: center; padding: 5px; border: 1px solid #e0e0e0; border-radius: 8px; background-color: #f9f9f9; text-align: center; width: 100%;">
-			<i class="fas fa-calendar-alt" style="font-size: 73px; color: black; margin-right: 10px;"></i>
-			<div style="flex-grow: 1;">
-				<h3 style="font-size: 28px; margin: 0; text-align: center;"> Average Interviews per Day</h3>
-				<p style="font-size: 53px; color: black; margin: 0; text-align: center;">{rounded_average_day}</p>
-			</div>
-		</div>
-		""",
-            unsafe_allow_html=True,
-        )
-
-        ### Value box 5 ###
-    # Count number of enumerators
-    unique_enumerators = data[enumerator].nunique()
-
-    ### Value box 6 ###
-    # Count the number of interviews per interviewer
-    interviews_per_interviewer = (
-        data.groupby(enumerator, observed=False)
-        .size()
-        .reset_index(name="interviews_count")
-    )
-
-    # Calculate the average number of interviews per enumerator
-    average_interviews_per_interviewer = interviews_per_interviewer[
-        "interviews_count"
-    ].mean()
-
-    # Round the number of interviews per enumerator
-    rounded_average = round(average_interviews_per_interviewer, 2)
-
-    ### Value box 7 ###
-    average_interviews_per_day_per_enumerator = (
-        average_interviews_per_day / (unique_enumerators)
-        if unique_enumerators > 0
-        else 0
-    )
-    rounded_average_day_per_enumerator = round(
-        average_interviews_per_day_per_enumerator, 2
-    )
-
-    ### Value box 8 ###
-    # Calculate interviews left
-    interviews_left = total_goal - valid_interviews
-
-    #### Create second row of value boxes
-    # Create columns for the value boxes
-    col5, col6, col7, col8 = st.columns(spec=4, border=True)
-
-    # Value box 5: Number of Enumerators
-    with col5:
-        st.markdown(
-            f"""
-		<div style="display: flex; align-items: center; padding: 21px; border: 1px solid #e0e0e0; border-radius: 8px; background-color: #f9f9f9; text-align: center; width: 100%;">
-			<i class="fas fa-user" style="font-size: 73px; color: black; margin-right: 10px;"></i>
-			<div style="flex-grow: 1;">
-				<h3 style="margin: 0; font-size: 23px; text-align: center;">Number of Enumerators</h3>
-				<p style="font-size: 50px; color: black; margin: 0; text-align: center;">{unique_enumerators}</p>
-			</div>
-		</div>
-		""",
-            unsafe_allow_html=True,
-        )
-
-    # Value box 6: Average Interviews per Enumerator
-    with col6:
-        st.markdown(
-            f"""
-		<div style="display: flex; align-items: center; padding: 10px; border: 1px solid #e0e0e0; border-radius: 8px; background-color: #f9f9f9; text-align: center; width: 100%;">
-			<i class="fas fa-chart-line" style="font-size: 75px; color: black; margin-right: 10px;"></i>
-			<div style="flex-grow: 1;">
-				<h3 style="margin: 0; font-size: 25px; text-align: center;">Average Interviews per Enumerator</h3>
-				<p style="font-size: 47px; color: black; margin: 0; text-align: center;">{rounded_average}</p>
-			</div>
-		</div>
-		""",
-            unsafe_allow_html=True,
-        )
-
-    # Value box 7: Avg interviews per day per enumerator
-    with col7:
-        st.markdown(
-            f"""
-		<div style="display: flex; align-items: center; justify-content: center; padding: 5px; border: 1px solid #e0e0e0; border-radius: 8px; background-color: #f9f9f9; text-align: center; width: 100%;">
-			<i class="fas fa-calendar-check" style="font-size: 75px; color: black; margin-right: 10px;"></i>
-			<div style="flex-grow: 1; text-align: center;">
-				<h3 style="margin: 0; font-size: 24px;">Average Interviews per Day per Enumerator</h3>
-				<p style="font-size: 50px; color: black; margin: 0;">{rounded_average_day_per_enumerator}</p>
-			</div>
-		</div>
-		""",
-            unsafe_allow_html=True,
-        )
-
-    # Value box 8: Interviews Left
-    with col8:
-        st.markdown(
-            f"""
-		<div style="display: flex; align-items: center; justify-content: center; padding: 1px; border: 1px solid #e0e0e0; border-radius: 8px; background-color: #f9f9f9; text-align: center; width: 100%;">
-			<i class="fas fa-pen-to-square" style="font-size: 70px; color: black; margin-right: 10px;"></i>
-			<div style="flex-grow: 1; text-align: center;">
-				<h3 style="margin: 0; font-size: 25px;">Number of Interviews Left to Reach Goal</h3>
-				<p style="font-size: 50px; color: black; margin: 0;">{interviews_left}</p>
-			</div>
-		</div>
-		""",
-            unsafe_allow_html=True,
-        )
-
-    # Calculate todays date
-    today = datetime.now().strftime(
-        "%B %d, %Y"
-    )  # Format: Month day, year (e.g., August 22, 2024)
-
-    # Creat text
-    last_updated_text = f"Last update: {today}"
-
-    # Show text
-    st.write(last_updated_text)
