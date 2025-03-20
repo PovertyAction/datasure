d8f4efdebd82407283a7ef532b3eb8d11b8f7c81
iabaako <iabaako@poverty-action.org>
2025-03-20 08:25:09 +0000

    setup for custom metric

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,6 @@
 from .dataframe_utils import add_row, move_row, remove_row
+from .metric_utils import custom_metric
 
-__all__ = ["add_row", "move_row", "remove_row"]
+__all__ = ["add_row", "custom_metric", "move_row", "remove_row"]
 
 from .chart_utils import donut_chart  # noqa: F401
