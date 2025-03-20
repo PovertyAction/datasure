fa3b1939eafbbf7e1830ae8cc6da962c210a44de
iabaako <iabaako@poverty-action.org>
2025-03-20 08:25:09 +0000

    setup for custom metric

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,4 @@
 from .dataframe_utils import add_row, move_row, remove_row
+from .metric_utils import custom_metric
 
-__all__ = ["add_row", "move_row", "remove_row"]
+__all__ = ["add_row", "custom_metric", "move_row", "remove_row"]
