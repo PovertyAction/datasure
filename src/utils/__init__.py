1c20d20a4065c4b1f345c121131285c521940aae
iabaako <iabaako@poverty-action.org>
2025-03-26 09:02:13 +0000

    adding chart2 to charts

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,6 +1,5 @@
 from .dataframe_utils import add_row, move_row, remove_row
-from .metric_utils import custom_metric
 
-__all__ = ["add_row", "custom_metric", "move_row", "remove_row"]
+__all__ = ["add_row", "move_row", "remove_row"]
 
-from .chart_utils import donut_chart  # noqa: F401
+from .chart_utils import donut_chart, donut_chart2  # noqa: F401
