7b9e54e3511e5063a9ded3ba96949dd3efb82226
iabaako <iabaako@poverty-action.org>
2025-05-03 14:44:30 +0000

    setup for progress report

--- views/output_view_1.py
+++ views/output_view_1.py
@@ -60,7 +60,9 @@ with missing:
 
 with survey_progress:
     progress_report(
-        data=st.session_state[f"prepped_data{page_data_index}"], page_num=page_number
+        data=st.session_state[f"prepped_data{page_data_index}"],
+        setting_file=setting_file,
+        page_num=page_number,
     )
 
 with duplicates:
