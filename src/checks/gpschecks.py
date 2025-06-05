21574ac4343573818dcf14c7443438206eb8541d
iabaako <iabaako@poverty-action.org>
2025-06-05 19:06:04 +0000

    fix/ make correction to fix test failure

--- src/checks/gpschecks.py
+++ src/checks/gpschecks.py
@@ -480,8 +480,9 @@ def detect_outliers_with_clusters(df, gps_lat_col, gps_lon_col, clustering_col):
     pd.DataFrame
         The input dataframe with an additional column indicating outliers.
     """
-    # Drop rows with missing latitude values
-    df = df[df[gps_lat_col].notna()]
+    # Drop rows with missing latitude values or longitude values
+    df = df.dropna(subset=[gps_lat_col, gps_lon_col])
+    # df = df[df[gps_lat_col].notna()]
 
     # Group data by clustering_col and process each group separately
     grouped_df = df.groupby(clustering_col)
@@ -577,6 +578,20 @@ def calculate_gps_accuracy_statistics(
     pd.DataFrame
         A dataframe containing grouped GPS accuracy statistics.
     """
+    allowed_stats = [
+        "min",
+        "median",
+        "mean",
+        "max",
+        "std",
+        "25th percentile",
+        "75th percentile",
+        "95th percentile",
+    ]
+    # Validate the accuracy_stats_list
+    accuracy_stats_list = [
+        stat for stat in accuracy_stats_list if stat in allowed_stats
+    ]
     # update percentile statistics with numpy percentile function
     percentile_map = {
         "25th percentile": lambda x: np.percentile(x, 25),
