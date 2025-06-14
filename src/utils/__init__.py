754261ef10e7a92afc8a696d54e399394d0795f5
iabaako <iabaako@poverty-action.org>
2025-06-14 14:44:20 +0000

    removed old df util functions, added get_df_info // get_df_info returns a tuple of  info for a DataFrame

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,5 @@
 from .chart_utils import donut_chart, donut_chart2
-from .dataframe_utils import add_row, move_row, remove_row
+from .dataframe_utils import get_df_info
 from .duckdb_utils import (
     duckdb_get_aliases,
     duckdb_get_imported_datasets,
@@ -15,7 +15,6 @@ from .settings_utils import (
 )
 
 __all__ = [
-    "add_row",
     "donut_chart",
     "donut_chart2",
     "duckdb_get_aliases",
@@ -23,10 +22,9 @@ __all__ = [
     "duckdb_get_table",
     "duckdb_row_filter",
     "duckdb_save_table",
+    "get_df_info",
     "get_hash_id",
     "load_check_settings",
-    "move_row",
-    "remove_row",
     "save_check_settings",
     "trigger_save",
 ]
