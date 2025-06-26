d50db9bd752798f507a7a05f434c99791efb8906
iabaako <iabaako@poverty-action.org>
2025-06-26 08:28:21 +0000

    chore/added version info and date to start page //this will help know which version of the app is being used during beta testing

--- views/start_view.py
+++ views/start_view.py
@@ -132,6 +132,7 @@ st.set_page_config(
 
 _, page_canvas, _ = st.columns([0.1, 0.8, 0.1])
 with page_canvas:
+    st.write("version 0.1.1 26 Jul 2025")
     st.image("assets/LinkedIn Cover IPA20.png", use_container_width=True)
 
     st.title("Welcome to pyDMS")
