7ef22a3809bee4071aaeeb8d58095361f5cb3547
Niall Keleher <nkeleher@poverty-action.org>
2025-07-03 05:52:05 -0700

    update email

--- src/pydms/__init__.py
+++ src/pydms/__init__.py
@@ -7,7 +7,7 @@ in survey data collection projects.
 
 __version__ = "0.1.0"
 __author__ = "Innovations for Poverty Action"
-__email__ = "tech@poverty-action.org"
+__email__ = "researchsupport@poverty-action.org"
 
 # Package metadata
 __title__ = "pyDMS"
