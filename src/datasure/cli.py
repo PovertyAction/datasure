6f4102f3f180e1e58cb494cf7f176bb402fd4e98
iabaako <iabaako@poverty-action.org>
2025-07-31 10:48:25 +0000

    remove sys.exit

--- src/datasure/cli.py
+++ src/datasure/cli.py
@@ -9,7 +9,7 @@ import argparse
 import sys
 from pathlib import Path
 
-import streamlit.web.cli as stcli
+import streamlit.web.cli as stcli  # noqa: F401
 
 
 def main():
@@ -62,9 +62,6 @@ def main():
         "--browser.gatherUsageStats",
         "false",
     ]
-
-    sys.exit(stcli.main())
-
 
 def get_version():
     """Get the package version."""
