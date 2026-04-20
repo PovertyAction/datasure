0688ce1336c5467085803f7201af44b6fa370a1c
iabaako <iabaako@poverty-action.org>
2026-04-20 15:04:14 +0000

    remove r and py generators

--- src/datasure/processing/replication/__init__.py
+++ src/datasure/processing/replication/__init__.py
@@ -1,1 +1,8 @@
+"""Stata replication package generation for DataSure."""
 
+from datasure.processing.replication.package_builder import build_replication_package
+from datasure.processing.replication.prep_script_generator import (
+    generate_prepare_data_script,
+)
+
+__all__ = ["build_replication_package", "generate_prepare_data_script"]
